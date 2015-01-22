0b4bbe336953452383b0ee4677527dd2619dc1c9
Roy Clarkson <rclarkson@pivotal.io>
2015-01-21 22:08:02 -0600

    Add initial code

---
+++ initial/src/main/java/demo/HomeController.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello World";
+  }
+
+}
